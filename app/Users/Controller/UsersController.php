693f77ed23a2cab2193ed2df82fa16e2351deffe
Pedro Nunes <pedro.nunesmoura@outlook.com>
2023-09-14 15:07:10 -0300

    Refactoring information related to the presentation of the user list.

--- app/Users/Controller/UsersController.php
+++ app/Users/Controller/UsersController.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace App\Users\Controller;
 
-use Pnmoura\Deliveryzefood\ListUsers\ConnectionDatabase;
+use Pnmoura\Deliveryzefood\ListUsers\ListUsers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class UsersController extends ConnectionDatabase
+class UsersController extends ListUsers
 {
     public function listUsers(Request $request, Response $response)
     {
-        $connection = new ConnectionDatabase();
+        $connection = new ListUsers();
         $list = $connection->listRegisters();
 
         $response->getBody()->write(
